c0726c6797ea8c7ce78d9d07adfb8cac9cb24427
Omar Fuentes Briones <omarsefuentes@gmail.com>
2022-09-28 15:38:56 -0500

    Calculadora.2

--- calculadora.py
+++ calculadora.py
@@ -13,22 +13,35 @@ def multiplicar(num1, num2):
 def divide(num1, num2):
     return num1/num2
 
+def return_values():
+    num1 = int(input("Selecciona un numero: "))#pedir numero
+    num2 = int(input("Selecciona otro un numero: "))#pedir otro numero
+    return [num1, num2] #regresar umeros 
+
 if __name__ == '__main__':
     message = f"calculadora: \n Elige una opcion\n1 - suma\n2 - resta\n3 - multiplicar\n4 - divide\n"
     while True:
         opcion = int(input(message))
         #comparar cada una de las opciones y llamar a la funcion correcta 
         if opcion == 1:
-            resultado_suma = suma(23, 66)
+            #Pedir numeros al usuario  
+            numeros = return_values()
+            resultado_suma = suma(numeros[0], numeros[1])
             print("El resultado de la suma es", resultado_suma)
         elif opcion == 2:
-            resultado_resta = resta(23, 66)
+            #Pedir numeros al usuario 
+            numeros = return_values()
+            resultado_resta = resta(numeros[0], numeros[1])
             print("El resultado de la resta es", resultado_resta)
         elif opcion == 3:
-            resultado_multiplicar = multiplicar(23, 66)
+            #Pedir numeros al usuario 
+            numeros = return_values()
+            resultado_multiplicar = multiplicar(numeros[0], numeros[1])
             print("El resultado de la multiplicacion es", resultado_multiplicar)
         elif opcion == 4:
-            resultado_divide = divide(23, 66)
+            #Pedir numeros al usuario 
+            numeros = return_values()
+            resultado_divide = divide(numeros[0], numeros[1])
             print("El resultado de la division es", resultado_divide)
         elif opcion == 5:
             print('Bye!!')
